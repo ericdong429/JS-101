5d4434d1e28d9c6392cade3df22b6042c0d3b315
Eric Dong <dongeric429@gmail.com>
2024-04-18 23:16:07 -0700

    rockpaperscissor.js edited

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,20 +1,36 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const ALT_CHOICES = ['r', 'p', 's', 'l', 'sk'];
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function alternativeChoices(choice) {
+  switch (choice) {
+    case 'r': return 'rock';
+    case 'p': return 'paper';
+    case 's': return 'scissors';
+    case 'l': return 'lizard';
+    case 'sk': return 'spock';
+    default: return choice;
+  }
+}
+
 function displayWinner(choice, computerChoice) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-      (choice === 'paper' && computerChoice === 'rock') ||
-      (choice === 'scissors' && computerChoice === 'paper')) {
+  if ((alternativeChoices(choice) === 'rock' && (computerChoice === 'scissors' || 'lizard')) ||
+      (alternativeChoices(choice) === 'paper' && (computerChoice === 'rock' || 'spock')) ||
+      (alternativeChoices(choice) === 'scissors'  && (computerChoice === 'paper' || 'lizard')) ||
+      (alternativeChoices(choice) === 'lizard' && (computerChoice === 'spock' || 'paper')) ||
+      (alternativeChoices(choice) === 'spock' && (computerChoice === 'scissors' || 'rock'))) {
     prompt('You win!');
-  } else if ((choice === 'rock' && computerChoice === 'paper') ||
-             (choice === 'paper' && computerChoice === 'scissors') ||
-             (choice === 'scissors' && computerChoice === 'rock')) {
+  } else if ((alternativeChoices(choice) === 'rock' && (computerChoice === 'paper' || 'spock')) ||
+             (alternativeChoices(choice) === 'paper' && (computerChoice === 'scissors' || 'lizard')) ||
+             (alternativeChoices(choice) === 'scissors' && (computerChoice === 'rock' || 'spock')) ||
+             (alternativeChoices(choice) === 'lizard' && (computerChoice === 'rock' || 'scissors')) ||
+             (alternativeChoices(choice) === 'spock' && (computerChoice === 'paper' || 'lizard'))) {
     prompt('Computer wins!');
   } else {
     prompt("It's a tie");
@@ -22,10 +38,11 @@ function displayWinner(choice, computerChoice) {
 }
 
 while (true) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')} 
+  or short hand choices ${ALT_CHOICES.join(', ')} (*sk = spock)`);
   let choice = readline.question();
 
-  while (!VALID_CHOICES.includes(choice)) {
+  while (!VALID_CHOICES.includes(alternativeChoices(choice))) {
     prompt("That's not a valid choice");
     choice = readline.question();
   }
